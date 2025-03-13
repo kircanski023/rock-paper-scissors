8a0708b26de3fc3cc7f0dd3db7480c98a883e2dd
Slobodan <kircanskis@gmail.com>
2025-03-13 21:07:14 +0100

    Write a function that returns a random string of Rock Paper or Scissors

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+//Write a function that returns a random string of "Rock" "Paper" or "Scissors"
+function getComputerResult(){
+    let result;
+    switch(Math.floor((Math.random() * 3) + 1 )) {
+        case 1:
+          result = "Rock";
+          break;
+        case 2:
+          result = "Paper";
+          break;
+        case 3:
+          result = "Scissors";
+      }
+    return console.log(result)
+}
+
+getComputerResult();
